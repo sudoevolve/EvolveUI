8aa434b628859cd0fdde47dd23a5b9a9dfdb4a34
Aino <147291183+sudoevolve@users.noreply.github.com>
2025-11-01 16:58:27 +0800

    add icon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QtQml>
+#include <QIcon>
 #include "core/music.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    // 设置应用程序图标
+    app.setWindowIcon(QIcon(":/new/prefix1/fonts/icon.ico"));
 
     // 注册C++类型到QML
     qmlRegisterType<AudioMetadata>("AudioMetadata", 1, 0, "AudioMetadata");
